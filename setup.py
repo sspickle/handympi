93d96b019003c98f928794fe3f8d850570ad7b14
Steve Spicklemire <steve@spvi.com>
2019-06-07 06:33:15 -0400

    prepare for packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(name='handympi',
+      version='0.1',
+      description='A simple framework for easy mpi',
+      url='http://github.com/sspickle/handympi',
+      author='Steve Spicklemire',
+      author_email='steve@spvi.com',
+      license='MIT',
+      packages=['handympi'],
+      zip_safe=False)
+
