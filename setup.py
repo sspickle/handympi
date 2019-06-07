401e64df41da6ea2c6416ee83624a2e8cb4a85af
Steve Spicklemire <steve@spvi.com>
2019-06-07 09:32:12 -0400

    need numpy as a dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='handympi',
-      version='0.22',
+      version='0.23',
       description='A simple framework for easy mpi',
       url='http://github.com/sspickle/handympi',
       author='Steve Spicklemire',
@@ -9,7 +9,7 @@ setup(name='handympi',
       license='MIT',
       packages=['handympi'],
       install_requires=[
-          'mpi4py',
+          'mpi4py','numpy',
       ],
       zip_safe=False)
 
