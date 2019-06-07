cd161e80c74a3e74bf4a67584a81683cad66c0d3
Steve Spicklemire <steve@spvi.com>
2019-06-07 06:46:16 -0400

    add dependency mpi4py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
+from setuptools import setup
+
 setup(name='handympi',
-      version='0.1',
+      version='0.2',
       description='A simple framework for easy mpi',
       url='http://github.com/sspickle/handympi',
       author='Steve Spicklemire',
       author_email='steve@spvi.com',
       license='MIT',
       packages=['handympi'],
+      install_requires=[
+          'mpi4py',
+      ],
       zip_safe=False)
 
